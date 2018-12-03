0a1f41e039bdef75a85ad5117393394dab797341
Joshua Flood <info@joshuaflood.com>
2018-12-03 10:08:03 +0000

    Create jcfResizeFont.js

---
+++ public/assets/js/jcfResizeFont.js
@@ -0,0 +1,16 @@
+function jcfResizeFont() {
+    var elements  = document.getElementsByClassName('jcfResize');
+    if (elements.length < 0) {
+        return;
+    }
+    _len = elements.length;
+    for (_i = 0; _i < _len; _i++) {
+        var el = elements[_i];
+        el.style.fontSize = "100%";
+        for (var size = 100; el.scrollHeight > el.clientHeight; size -= 10) {
+            el.style.fontSize = size + '%';
+        }
+    }
+}
+
+window.addEventListener('resize', jcfResizeFont); 
